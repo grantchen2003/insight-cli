b5ae5059e0f83280d8da061ce2345fb924cff7d3
Productivity <grantchen2021@gmail.com>
2023-11-12 22:48:47 -0500

    fixed typo in core __init__.py file

--- src/insight-cli/core/__init__.py
+++ src/insight-cli/core/__init__.py
@@ -1,3 +1,3 @@
 from .repository import initialize, reinitialize, uninitialize
-from .insight_dir import get_codebase_id, is_valid, create, delete
+from .insight_dir import get_repository_id, is_valid, create, delete
 from .insightignore_file import get_ignorable_names
