120585a3d4773ab9033de8c942b03384cbcc3c43
Productivity <grantchen2021@gmail.com>
2023-12-04 20:19:09 -0500

    added more tests for query_command

--- tests/test_commands/test_query_command.py
+++ tests/test_commands/test_query_command.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 import contextlib, io, unittest
 
 from insight_cli.commands import QueryCommand
@@ -67,6 +68,36 @@ class TestQueryCommand(unittest.TestCase):
                 "/server/insight_cli/server.js",
                 f"\tLine 25: {Color.green('await connectToDatabase(app);')}",
             ],
+        )
+
+    @patch("insight_cli.commands.QueryCommand._print_matches")
+    @patch("insight_cli.repository.Repository.query")
+    def test_execute_with_valid_repository(
+        self, mock_repository_query, mock_print_matches
+    ) -> None:
+        query_command = QueryCommand()
+        query_string = "sample_query_string"
+
+        query_command.execute(query_string)
+
+        mock_repository_query.assert_called_once_with(query_string)
+        mock_print_matches.assert_called_once()
+
+    @patch("builtins.print")
+    @patch("insight_cli.repository.Repository.query")
+    def test_execute_with_no_repository_found(
+        self, mock_repository_query, mock_print
+    ) -> None:
+        query_command = QueryCommand()
+        query_string = "sample_query_string"
+
+        mock_repository_query.side_effect = FileNotFoundError()
+
+        query_command.execute(query_string)
+
+        mock_repository_query.assert_called_once_with(query_string)
+        mock_print.assert_called_once_with(
+            Color.red("The current directory is not an insight repository.")
         )
 
 
