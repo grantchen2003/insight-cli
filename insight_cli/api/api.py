9f5b5dcb5f3cb9c9e2b69a45ce1b49a03aef5e57
Productivity <grantchen2021@gmail.com>
2023-12-06 21:09:36 -0500

    need to fix api more

--- insight_cli/api/api.py
+++ insight_cli/api/api.py
@@ -6,23 +6,28 @@ from insight_cli.utils.directory import Directory
 
 class API:
     @staticmethod
-    def make_initialize_repository_request(
-            repository_dir: Directory
-    ) -> dict[str, str]:
+    def make_initialize_repository_request(repository_dir: Directory) -> dict[str, str]:
         request_url = f"{config.INSIGHT_API_BASE_URL}/initialize_repository"
 
         request_json_body = json.dumps(
-            {"repository_dir": repository_dir.to_dict()}, default=str
+            {"repository_dir": repository_dir.to_dict()},
+            default=str,
         )
 
-        response = requests.post(url=request_url, json=request_json_body)
+        response = requests.post(
+            headers={"Content-Type": "application/json"},
+            url=request_url,
+            json=request_json_body,
+        )
 
         response.raise_for_status()
 
         return response.json()
 
     @staticmethod
-    def make_query_repository_request(repository_id: str, query_string: str) -> list[dict]:
+    def make_query_repository_request(
+        repository_id: str, query_string: str
+    ) -> list[dict]:
         request_url = f"{config.INSIGHT_API_BASE_URL}/query?repository-id={repository_id}&query-string={query_string}"
 
         response = requests.get(url=request_url)
@@ -38,7 +43,10 @@ class API:
         request_url = f"{config.INSIGHT_API_BASE_URL}/reinitialize_repository"
 
         request_json_body = json.dumps(
-            {"repository_dir": repository_dir.to_dict(), "repository_id": repository_id},
+            {
+                "repository_dir": repository_dir.to_dict(),
+                "repository_id": repository_id,
+            },
             default=str,
         )
 
