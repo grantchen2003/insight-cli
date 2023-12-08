d3932fa91088624db36c3798973a4d65da802859
Productivity <grantchen2021@gmail.com>
2023-12-07 23:45:46 -0500

    removed time from main

--- insight_cli/main.py
+++ insight_cli/main.py
@@ -4,11 +4,8 @@ from insight_cli.commands import QueryCommand
 from insight_cli.commands import UninitializeCommand
 from insight_cli.commands import VersionCommand
 
-import time
-
 
 def main() -> None:
-    start = time.perf_counter()
     cli = CLI(
         commands=[
             InitializeCommand(),
@@ -22,7 +19,7 @@ def main() -> None:
     cli.parse_arguments()
 
     cli.execute_invoked_commands()
-    print(f"time: {time.perf_counter() - start}")
+
 
 if __name__ == "__main__":
     main()
