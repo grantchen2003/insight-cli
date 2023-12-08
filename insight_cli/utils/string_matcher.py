4745373e50ae88d75481943cd439ff57f628f192
Productivity <grantchen2021@gmail.com>
2023-12-07 23:42:44 -0500

    added string_matcher

--- insight_cli/utils/string_matcher.py
+++ insight_cli/utils/string_matcher.py
@@ -1,24 +1,27 @@
-import functools, re
+import re
 
 
 class StringMatcher:
-    @staticmethod
-    def _convert_list_args_to_tuple(func):
-        def wrapper(*args, **kwargs):
-            args = [tuple(x) if isinstance(x, list) else x for x in args]
-            return func(*args, **kwargs)
+    _cache = {"_raise_for_invalid_regex_patterns": {}}
 
-        return wrapper
+    @classmethod
+    def _raise_for_invalid_regex_patterns(cls, patterns: list[str]) -> None:
+        cache = cls._cache["_raise_for_invalid_regex_patterns"]
+        patterns = tuple(patterns)
 
-    @staticmethod
-    @_convert_list_args_to_tuple
-    @functools.lru_cache(maxsize=None)
-    def _raise_for_invalid_regex_patterns(patterns: list[str]) -> None:
-        for pattern in patterns:
-            try:
-                re.compile(pattern)
-            except re.error as e:
-                raise ValueError(f"{pattern} is an invalid regex pattern. {e}")
+        if patterns not in cache:
+            cache[patterns] = None
+
+            for pattern in patterns:
+                try:
+                    re.compile(pattern)
+                except re.error as e:
+                    cache[patterns] = ValueError(
+                        f"{pattern} is an invalid regex pattern. {e}"
+                    )
+
+        if cache[patterns]:
+            raise cache[patterns]
 
     @staticmethod
     def matches_any_regex_pattern(query_string: str, regex_patterns: list[str]) -> bool:
