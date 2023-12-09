d7e661a077da15c48a00b89ebac197e4b9f8dbf3
Productivity <grantchen2021@gmail.com>
2023-12-08 19:02:40 -0500

    replaced name property with path for File

--- insight_cli/utils/file.py
+++ insight_cli/utils/file.py
@@ -3,27 +3,25 @@ from typing import TypedDict
 
 
 class FileDict(TypedDict):
-    name: str
+    path: Path
     lines: list[str]
 
 
 class File:
-    def __init__(self, name: str, lines: list[str]):
-        self._name: str = name
+    def __init__(self, path: Path, lines: list[str]):
+        self._path: Path = path
         self._lines: list[str] = lines
 
     @staticmethod
-    def create_in_file_system(parent_dir_path: Path, file_dict: FileDict) -> None:
-        file_path: Path = parent_dir_path / file_dict["name"]
-
-        with open(file_path, "w") as file:
+    def create_in_file_system(file_dict: FileDict) -> None:
+        with open(file_dict["path"], "w") as file:
             file.write("\n".join(file_dict["lines"]))
 
     @staticmethod
     def create_from_path(file_path: Path) -> "File":
         with open(file_path, "rb") as file:
             lines = file.read().decode("utf-8", errors="ignore").splitlines()
-        return File(name=file_path.name, lines=lines)
+        return File(path=file_path, lines=lines)
 
     def to_dict(self) -> FileDict:
-        return {"name": self._name, "lines": self._lines}
+        return {"path": str(self._path), "lines": self._lines}
