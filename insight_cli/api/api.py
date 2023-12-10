73f0ba35bf3b0f1d5e3226559c8cda6b9395c5b9
Productivity <grantchen2021@gmail.com>
2023-12-10 01:14:16 -0500

    attempted to speed up file upload to server by multithreading and batching with each batch having the same number of files

--- insight_cli/api/api.py
+++ insight_cli/api/api.py
@@ -1,4 +1,5 @@
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 from insight_cli import config
 
@@ -33,13 +34,30 @@ class API:
     def make_reinitialize_repository_request(
         repository_files: dict[str:bytes], repository_id: str
     ) -> None:
-        response = requests.post(
-            url=f"{config.INSIGHT_API_BASE_URL}/reinitialize_repository",
-            files=repository_files,
-            json={"repository_id": repository_id},
-        )
+        NUM_FILES_PER_BATCH = 10  # Adjust this based on your needs
 
-        response.raise_for_status()
+        def upload_batch(batch_files):
+            response = requests.post(
+                url=f"{config.INSIGHT_API_BASE_URL}/reinitialize_repository",
+                files=batch_files,
+                json={"repository_id": repository_id},
+            )
+            response.raise_for_status()
+
+        with ThreadPoolExecutor() as executor:
+            batch_files = {}
+            futures = []
+            for i, (file_path, file_data) in enumerate(repository_files.items()):
+                batch_files[file_path] = file_data
+                if (i + 1) % NUM_FILES_PER_BATCH == 0 or (i + 1) == len(
+                    repository_files
+                ):
+                    # When the batch is full or it's the last file, submit the batch for upload
+                    futures.append(executor.submit(upload_batch, batch_files))
+                    batch_files = {}  # Start a new batch
+
+            for future in futures:
+                future.result()
 
     @staticmethod
     def make_validate_repository_id_request(repository_id: str) -> dict[str, bool]:
