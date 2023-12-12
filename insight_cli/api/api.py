9b1d84a44b2d13467d4542c3260c157a8b4f176c
Productivity <grantchen2021@gmail.com>
2023-12-11 20:51:00 -0500

    added multithreading and batching for init request

--- insight_cli/api/api.py
+++ insight_cli/api/api.py
@@ -9,36 +9,52 @@ class API:
     def make_initialize_repository_request(
         repository_files: dict[str:bytes],
     ) -> dict[str, str]:
-        response = requests.post(
-            url=f"{config.INSIGHT_API_BASE_URL}/initialize_repository",
-            files=repository_files,
-        )
+        def make_batch_request(payload):
+            response = requests.post(
+                url=f"{config.INSIGHT_API_BASE_URL}/initialize_repository",
+                files=payload["files"],
+            )
 
-        response.raise_for_status()
+            response.raise_for_status()
 
-        return response.json()
+            return response.json()
 
-    @staticmethod
-    def make_query_repository_request(
-        repository_id: str, query_string: str
-    ) -> list[dict]:
-        response = requests.get(
-            url=f"{config.INSIGHT_API_BASE_URL}/query?repository-id={repository_id}&query-string={query_string}"
-        )
+        BYTES_PER_BATCH = 10 * 1024 * 1024
 
-        response.raise_for_status()
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            batch_payload = {"files": {}}
+            batch_size = 0
+            futures = []
 
-        return response.json()
+            for file_path, file_content in repository_files.items():
+                file_size = len(file_content)
+
+                if batch_size + file_size > BYTES_PER_BATCH and batch_payload:
+                    futures.append(executor.submit(make_batch_request, batch_payload))
+                    batch_payload = {"files": {}}
+                    batch_size = 0
+
+                batch_payload["files"][file_path] = file_content
+                batch_size += file_size
+
+            if batch_payload:
+                futures.append(executor.submit(make_batch_request, batch_payload))
+
+            response_json = {}
+            for future in futures:
+                response_json.update(future.result())
+
+            return response_json
 
     @staticmethod
     def make_reinitialize_repository_request(
-        repository_files: dict[str:tuple[bytes, str]], repository_id: str
+        repository_files: dict[str : tuple[bytes, str]], repository_id: str
     ) -> None:
         def make_batch_request(payload):
             response = requests.post(
                 url=f"{config.INSIGHT_API_BASE_URL}/reinitialize_repository",
                 files=payload["files"],
-                data={"repository_id": repository_id, **payload["actions"]}
+                data={"repository_id": repository_id, **payload["actions"]},
             )
             response.raise_for_status()
 
@@ -57,7 +73,7 @@ class API:
                     futures.append(executor.submit(make_batch_request, batch_payload))
                     batch_payload = {"files": {}, "actions": {}}
                     batch_size = 0
-                
+
                 if file_action != "delete":
                     batch_payload["files"][file_path] = file_content
                 batch_payload["actions"][file_path] = file_action
@@ -75,6 +91,18 @@ class API:
             headers={"Content-Type": "application/json"},
             url=f"{config.INSIGHT_API_BASE_URL}/validate_repository_id",
             json={"repository_id": repository_id},
+        )
+
+        response.raise_for_status()
+
+        return response.json()
+
+    @staticmethod
+    def make_query_repository_request(
+        repository_id: str, query_string: str
+    ) -> list[dict]:
+        response = requests.get(
+            url=f"{config.INSIGHT_API_BASE_URL}/query?repository-id={repository_id}&query-string={query_string}"
         )
 
         response.raise_for_status()
