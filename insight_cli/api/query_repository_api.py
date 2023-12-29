4f9fbd259991f021d21406db1c5bd9148aa06fed
Productivity <grantchen2021@gmail.com>
2023-12-29 13:38:54 -0500

    updated query repo api url path

--- insight_cli/api/query_repository_api.py
+++ insight_cli/api/query_repository_api.py
@@ -8,7 +8,7 @@ class QueryRepositoryAPI(API):
     @staticmethod
     def make_request(repository_id: str, query_string: str) -> list[dict]:
         response = requests.get(
-            url=f"{config.INSIGHT_API_BASE_URL}/query",
+            url=f"{config.INSIGHT_API_BASE_URL}/query_repository",
             json={
                 "repository_id": repository_id,
                 "query_string": query_string,
